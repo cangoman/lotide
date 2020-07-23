2c7d8dc589c247e72b47c221c1153e7bcd133bcd
Camilo <camilogomezmusic@gmail.com>
2020-07-23 19:04:02 +0000

    added findKey.js function, which takes an object and a callback as parameters anre returns the first key with which the calback function returns a truthy value

---
+++ findKey.js
@@ -0,0 +1,30 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  let result;
+  for (const key in object) {
+    if (callback(object[key])) {
+      result = key;
+      return result;
+    }
+  }
+  return result;
+};
+
+const object1 = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}; // => "noma"
+
+assertEqual(findKey(object1, x => x.stars > 3), undefined);
