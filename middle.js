88ad5d305cea761993244c906016164990ae8d68
Camilo Gomez <camilogomezmusic@gmail.com>
2020-07-27 14:06:02 +0000

    refactored middle.js, added tess in a separate file

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i])
-        return false;
-    }
-    return true;
-  } else
-    return false;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
-
 const middle = function(array) {
   if (array.length <= 2)
     return [];
@@ -27,5 +7,5 @@ const middle = function(array) {
   }
 };
 
-assertArraysEqual(middle([7,6]), []);
+module.exports = middle;
 
