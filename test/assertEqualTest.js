bcf7904d7c246a95ecc296183ec9e503c0c74562
Camilo Gomez <camilogomezmusic@gmail.com>
2020-07-27 13:49:45 +0000

    refactored assertArraysEqual.js and added a separate file for its tests

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,7 +1,6 @@
 const assertEqual = require('../assertEqual');
 
 
-
 assertEqual("Lighthouse Labs", "Lighthouse"); // => Should fail
 assertEqual(1, 1); // => should pass
 assertEqual("Bootycamp", "Bootcamp"); // => Should fail
