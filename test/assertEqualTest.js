6563e1c855aa9fbfc9bb36d6c8cd3cf1040dbb0c
Camilo Gomez <camilogomezmusic@gmail.com>
2020-07-27 13:32:02 +0000

    Refactore eqArrays.js, added a separate file from tests. Also linted all the test files

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -3,7 +3,7 @@ const assertEqual = require('../assertEqual');
 
 
 assertEqual("Lighthouse Labs", "Lighthouse"); // => Should fail
-assertEqual(1, 1); // => should pass 
+assertEqual(1, 1); // => should pass
 assertEqual("Bootycamp", "Bootcamp"); // => Should fail
-assertEqual("Bootcamp", "Bootcamp"); // => should pass 
+assertEqual("Bootcamp", "Bootcamp"); // => should pass
 assertEqual(1, 3); // => Should fail
--- test/tailTest.js
+++ test/tailTest.js
@@ -2,14 +2,14 @@ const assertEqual = require("../assertEqual");
 const tail = require("../tail");
 
 const numbers = [1, 5, 10, 20];
-let num_tail = tail(numbers);
-assertEqual(5, num_tail[0]);
+let numTail = tail(numbers);
+assertEqual(5, numTail[0]);
 
-num_tail = tail(num_tail);
-assertEqual(10, num_tail[0]);
+numTail = tail(numTail);
+assertEqual(10, numTail[0]);
 
-num_tail = tail(num_tail);
-assertEqual(20, num_tail[0]);
+numTail = tail(numTail);
+assertEqual(20, numTail[0]);
 
 
 
